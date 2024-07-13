ab0e24ce7e7fe461d75e1c39087ff751813bdeee
Leonardo-Chappie <leomeireles66@gmail.com>
2024-07-13 08:39:55 -0300

    Building the classes Account and Client.

--- src/model/entities/Account.java
+++ src/model/entities/Account.java
@@ -1,5 +1,46 @@
 package model.entities;
 
 public class Account {
+	private Integer numberAccount;
+	private Double balance;
+	private Client client;
+	
+	
+	public Account() {
+	}
 
+	public Account(Integer numberAccount, Double balance, Client client) {
+		this.client = client;
+		this.numberAccount = numberAccount;
+		this.balance = balance;
+	}
+
+	public Integer getNumberAccount() {
+		return numberAccount;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	
+	public Client getClient() {
+		return client;
+	}
+	
+	public void deposit(Double amount) {
+		this.balance += amount;
+	}
+	
+	public void withdraw(Double amount) {
+		this.balance -= amount;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Number accoount: " + getNumberAccount() + "\n");
+		sb.append("Balance: " + getBalance() + "\n");
+		sb.append("Client: " + getClient() + "\n");
+		return sb.toString();
+	}
 }
--- src/model/entities/Client.java
+++ src/model/entities/Client.java
@@ -1,5 +1,41 @@
 package model.entities;
 
 public class Client {
+	private String cpfClient;
+	private String nameClient;
+	private Integer phoneNumberClient;
+	
+	public Client() {
+	}
 
+	public Client(String cpfClient, String nameClient, Integer phoneNumberClient) {
+		this.cpfClient = cpfClient;
+		this.nameClient = nameClient;
+		this.phoneNumberClient = phoneNumberClient;
+	}
+
+	public String getNameClient() {
+		return nameClient;
+	}
+
+	public void setNameClient(String nameClient) {
+		this.nameClient = nameClient;
+	}
+
+	public Integer getPhoneNumberClient() {
+		return phoneNumberClient;
+	}
+
+	public void setPhoneNumberClient(Integer phoneNumberClient) {
+		this.phoneNumberClient = phoneNumberClient;
+	}
+
+	public String getCpfClient() {
+		return cpfClient;
+	}
+	
+	@Override
+	public String toString() {
+		return getCpfClient() + " " + getNameClient() + " " + getPhoneNumberClient();
+	}
 }
