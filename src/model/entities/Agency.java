63a759d6c84d920b015cd344dda80f311d5ede7f
Leonardo-Chappie <leomeireles66@gmail.com>
2024-07-13 10:24:24 -0300

    Builded the class Agency.

--- src/model/entities/Agency.java
+++ src/model/entities/Agency.java
@@ -1,5 +1,60 @@
 package model.entities;
 
-public class Agency {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Agency {
+	private Integer agencyCode;
+	private String bankName;
+	private String agencyAddress;
+	private List<Account> accounts = new ArrayList<>();
+	
+	public Agency() {
+	}
+
+	public Agency(Integer agencyCode, String bankName, String agencyAddress) {
+		this.agencyCode = agencyCode;
+		this.bankName = bankName;
+		this.agencyAddress = agencyAddress;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public String getAgencyAddress() {
+		return agencyAddress;
+	}
+
+	public void setAgencyAddress(String agencyAddress) {
+		this.agencyAddress = agencyAddress;
+	}
+
+	public Integer getAgencyCode() {
+		return agencyCode;
+	}
+	
+	public void setAgencyCode(Integer agencyCode) {
+		this.agencyCode = agencyCode;
+	}
+
+	public void addAccount(Account account) {
+		accounts.add(account);
+	}
+	
+	public void removeAccount(Account account) {
+		accounts.remove(account);
+	}
+	
+	@Override
+	public String toString() {
+		return "Bank name: " + getBankName() + 
+				" Agency code: " + getAgencyCode() + 
+				" Agency address: " + getAgencyAddress();
+	}
+	
 }
